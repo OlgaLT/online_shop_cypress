a46fe6e78a3069294141b255b0ab31b9325b6dd8
silkdemon <silkdemon@yandex.ru>
2024-10-11 13:31:15 +0300

    kek

--- cypress/pages/checkOutElements.ts
+++ cypress/pages/checkOutElements.ts
@@ -11,7 +11,6 @@ class checkOutElements {
   public static readonly chequePayment = '.cheque';
   public static readonly successAlert = '.alert-success';
   public static readonly successAlertText: string = 'Your order on My Shop is complete.'
-
 }
 
 export default checkOutElements
